312e9fb64bf97333ac066f4c5b1615d886aaf2af
Juan Penaloza <juan.pa097@gmail.com>
2023-07-08 16:49:30 +0100

    revamp the form

--- app/create-event/page.tsx
+++ app/create-event/page.tsx
@@ -1,133 +1,152 @@
 "use client";
 
 import { addDoc, collection, Timestamp } from 'firebase/firestore';
-import Image from 'next/image'
-import { useState } from 'react';
+import { PlusOutlined } from '@ant-design/icons';
 import { firestore, storage } from '../services/FirebaseService';
 import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { Button, Card, DatePicker, Form, Input, Layout, message, Typography, Upload } from 'antd';
+import type { UploadFile } from 'antd/es/upload/interface';
+import type { UploadRequestOption as RcCustomRequestOptions } from 'rc-upload/lib/interface';
+import type { Dayjs } from 'dayjs';
+const { Content } = Layout;
+const { TextArea } = Input;
 
 export default function CreateEvent() {
 
-  const [name, setName] = useState('');
-  const [location, setLocation] = useState('');
-  const [description, setDescription] = useState('');
-  const [date, setDate] = useState('');
-  const [address, setAddress] = useState('');
-  const [picture, setPicture] = useState<File | null>(null);
-  const [pictureUrl, setPictureUrl] = useState<string | null>(null);
+  const [messageApi, contextHolder] = message.useMessage();
 
-  const handlePictureUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
+  type FormValues = {
+    name: string;
+    location_name: string;
+    description: string;
+    files: UploadFile[],
+    date: Dayjs;
+    address: string;
+    image_url: string;
+  }
 
-    if (file) {
-      // Upload the picture to Firebase Storage
-      const storageRef = ref(storage, `pictures/${file.name}`);
-      await uploadBytes(storageRef, file);
+  type EventModel = {
+    name: string;
+    location_name: string;
+    description: string;
+    date: Timestamp;
+    address: string;
+    image_url: string;
+  }
 
-      // Get the download URL of the uploaded picture
-      const downloadURL = await getDownloadURL(storageRef);
-      setPictureUrl(downloadURL);
+  const [form] = Form.useForm();
+
+  const onFinish = async (values: FormValues) => {
+
+    const eventModel: EventModel = {
+      name: values.name,
+      location_name: values.location_name,
+      description: values.description,
+      date: Timestamp.fromDate(values.date?.toDate() ?? new Date()),
+      address: values.address,
+      image_url: values.files[0].response
     }
+    try {
+      await addDoc(collection(firestore, 'events'), eventModel);
+      messageApi.open({
+        type: 'success',
+        content: 'Event created!',
+      });
+      form.resetFields();
+
+    } catch {
+      onFinishFailed()
+    }
+
   };
 
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // You can perform any necessary form submission logic here
-    // For example, sending the data to a server or updating the state
-    console.log({
-      name,
-      location,
-      description,
-      date,
-      address,
-      pictureUrl,
+  const onFinishFailed = () => {
+    messageApi.open({
+      type: 'error',
+      content: 'Error creating event',
     });
+  };
 
-    const event = {
-      'name': name,
-      'location_name': location,
-      'description': description,
-      'date': Timestamp.fromDate(new Date(date)),
-      'address': address,
-      'image_url': pictureUrl,
-    }
-
+  const customUpload = async (options: RcCustomRequestOptions) => {
+    const file = options.file as File
+    const storageRef = ref(storage, `pictures/${file.name}`);
+    console.log(file);
     try {
-      await addDoc(collection(firestore, 'events'), event);
+      const result = await uploadBytes(storageRef, file);
+      const downloadURL = await getDownloadURL(storageRef);
+      console.log(result);
+      console.log(downloadURL);
+      options.onSuccess?.(downloadURL, new XMLHttpRequest());
     } catch (e: any) {
       console.log(e);
+      options.onError?.(e)
     }
-
-    // Reset form fields
-    setName('');
-    setLocation('');
-    setDescription('');
-    setDate('');
-    setAddress('');
-    setPicture(null);
-    setPictureUrl(null);
   };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Location:
-          <input
-            type="text"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Description:
-          <textarea
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Date:
-          <input
-            type="date"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Address:
-          <input
-            type="text"
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-          />
-        </label>
-        <br />
-        <label>
-          Picture:
-          <input type="file" onChange={handlePictureUpload} />
-        </label>
-        <br />
-        {pictureUrl && (
-          <div>
-            <Image src={pictureUrl} alt="Uploaded" />
-          </div>
-        )}
-        <br />
-        <button type="submit">Create Event</button>
-      </form>
-    </div>
-  )
+    <Layout>
+      <Content className='flex flex-col items-center justify-center h-screen'>
+        <Card title="Crear evento" className='w-1/3 shadow-lg text-center'>
+          <Form
+            name="createEvent"
+            form={form}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Name"
+              name="name"
+              rules={[{ required: true }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Location"
+              name="location_name"
+              rules={[{ required: true }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item
+              label="Description"
+              name="description"
+              rules={[{ required: true }]}
+            >
+              <TextArea
+                autoSize={{ minRows: 3, maxRows: 5 }}
+              />
+            </Form.Item>
+            <Form.Item
+              label="Start time"
+              name="date"
+              rules={[{ required: true }]}
+            >
+              <DatePicker showTime />
+            </Form.Item>
+            <Form.Item
+              label="Address"
+              name="address"
+              rules={[{ required: true }]}
+            >
+              <Input />
+            </Form.Item>
+            <Form.Item label="Upload picture" name="files" valuePropName="fileList">
+              <Upload customRequest={customUpload} listType="picture-card">
+                <div>
+                  <PlusOutlined />
+                  <div style={{ marginTop: 8 }}>Upload</div>
+                </div>
+              </Upload>
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      </Content>
+    </Layout>
+  );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 'use client';
 
 import React from "react";
-import { Button, DatePicker, Layout, Space, version, } from "antd";
+import { Button, Layout, } from "antd";
 import { Typography } from 'antd';
-const { Header, Footer, Sider, Content } = Layout;
+import Link from "next/link";
+const { Content } = Layout;
 
 const { Title, Text } = Typography;
 
@@ -12,10 +13,12 @@ export default function Home() {
     <main>
       <Layout>
         <Content className="flex flex-col items-center justify-center h-screen">
-          <Title level={2} className="m-0">Bienvenido a Teapptro!</Title>
+          <Title level={1} className="m-0">Bienvenido a Teapptro!</Title>
           <Text className="mb-3 w-1/3 text-center">Descubre una nueva forma de sumergirte en el mundo del teatro: ¡crea tus propios eventos, compártelos con tus amigos y vive experiencias teatrales inolvidables!</Text>
           <div>
-          <Button type="primary">Crear evento</Button>
+          <Link href="/create-event" passHref>
+            <Button type="primary">Crear evento</Button>
+          </Link>
           </div>
       </Content>
       </Layout>
